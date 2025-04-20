cae416019ed942235c1efc6d02493c689b1f5191
Reihan Saputra <sreihan513@gmail.com>
2025-04-21 01:01:27 +0700

    push

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -15,8 +15,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        $middleware->alias(["guest.login" => guestmiddleware::class]);
-        $middleware->alias(["user.login" => usermiddleware::class]);
+        //
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
--- routes/api.php
+++ routes/api.php
@@ -4,12 +4,9 @@ use App\Http\Controllers\catatancontroller;
 use App\Http\Controllers\usercontroller;
 use Illuminate\Support\Facades\Route;
 
-Route::middleware("guest.login")->group(function () {
     Route::post('/regist', [usercontroller::class, 'regist'])->name('regist');
     Route::post('/login', [usercontroller::class, 'login'])->name('login');
-});
 
-Route::middleware('user.login')->group(function() {
     Route::post('/edituser', [usercontroller::class, 'edituser'])->name('edituser');
     Route::post('/hapususer', [usercontroller::class, 'hapususer'])->name('hapususer');
     Route::get('/home',[catatancontroller::class, "index"])->name("home");
@@ -18,4 +15,3 @@ Route::middleware('user.login')->group(function() {
     Route::post('/hapuscatatan/{id}', [catatancontroller::class, 'hapuscatatan'])->name('hapuscatatan');
     Route::post('geteditcatatan/{id}',[catatancontroller::class,'geteditcatatan'])->name('geteditcatatan');
     Route::post('/logout', [usercontroller::class, 'logout'])->name('logout');
-});
